72b6378f438c4fa9d66a90037d70cbadd90e8fa0
TsabitFarel <tsabitzmail@gmail.com>
2021-11-13 08:35:11 +0700

    Initial Commit

---
+++ Shared/EmojiisApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiisApp.swift
+//  Shared
+//
+//  Created by Tsabit Farel on 13/11/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
